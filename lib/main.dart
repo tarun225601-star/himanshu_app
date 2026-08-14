1091b5b3c6fc6b059b086964638631feca77dbd8
tarun225601-star <tarun225601@gmail.com>
2026-08-14 12:19:08 +0530

    Autonomous Laborer: Update lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,29 @@ import 'package:provider/provider.dart';
 void main() {
   runApp(
     MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: const CalculatorApp(),
+      providers: [],
+      child: MyApp(),
     ),
   );
 }
-class CalculatorApp extends StatelessWidget {
-  const CalculatorApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return MaterialApp(
-          title: 'Calculator App',
-          theme: themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
-          home: const CalculatorPage(),
-        );
-      },
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Calculator(),
     );
   }
 }
-class CalculatorPage extends StatefulWidget {
-  const CalculatorPage({Key? key}) : super(key: key);
+class Calculator extends StatefulWidget {
   @override
-  State<CalculatorPage> createState() => _CalculatorPageState();
+  _CalculatorState createState() => _CalculatorState();
 }
-class _CalculatorPageState extends State<CalculatorPage> {
-  final _controller = TextEditingController();
+class _CalculatorState extends State<Calculator> {
+  String _expression = '';
   String _result = '';
   @override
   Widget build(BuildContext context) {
@@ -40,203 +34,283 @@ class _CalculatorPageState extends State<CalculatorPage> {
         children: [
           Expanded(
             child: Container(
-              padding: const EdgeInsets.all(20),
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.bottomRight,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: '0',
-                    ),
+                  Text(
+                    _expression,
+                    style: TextStyle(fontSize: 24),
                   ),
                   Text(
                     _result,
-                    style: const TextStyle(fontSize: 24),
+                    style: TextStyle(fontSize: 48),
                   ),
                 ],
               ),
             ),
           ),
           Expanded(
-            child: GridView.count(
-              crossAxisCount: 4,
-              childAspectRatio: 1.2,
-              children: [
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+            child: Container(
+              child: GridView.count(
+                crossAxisCount: 4,
+                childAspectRatio: 1.2,
+                children: [
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '7';
+                      });
+                    },
+                    child: Text('7'),
                   ),
-                  onPressed: () {
-                    _controller.text += '7';
-                  },
-                  child: const Text('7'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '8';
+                      });
+                    },
+                    child: Text('8'),
                   ),
-                  onPressed: () {
-                    _controller.text += '8';
-                  },
-                  child: const Text('8'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '9';
+                      });
+                    },
+                    child: Text('9'),
                   ),
-                  onPressed: () {
-                    _controller.text += '9';
-                  },
-                  child: const Text('9'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '/';
+                      });
+                    },
+                    child: Text('/'),
                   ),
-                  onPressed: () {
-                    _controller.text += '/';
-                  },
-                  child: const Text('/'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '4';
+                      });
+                    },
+                    child: Text('4'),
                   ),
-                  onPressed: () {
-                    _controller.text += '4';
-                  },
-                  child: const Text('4'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '5';
+                      });
+                    },
+                    child: Text('5'),
                   ),
-                  onPressed: () {
-                    _controller.text += '5';
-                  },
-                  child: const Text('5'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '6';
+                      });
+                    },
+                    child: Text('6'),
                   ),
-                  onPressed: () {
-                    _controller.text += '6';
-                  },
-                  child: const Text('6'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '*';
+                      });
+                    },
+                    child: Text('*'),
                   ),
-                  onPressed: () {
-                    _controller.text += '*';
-                  },
-                  child: const Text('*'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '1';
+                      });
+                    },
+                    child: Text('1'),
                   ),
-                  onPressed: () {
-                    _controller.text += '1';
-                  },
-                  child: const Text('1'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '2';
+                      });
+                    },
+                    child: Text('2'),
                   ),
-                  onPressed: () {
-                    _controller.text += '2';
-                  },
-                  child: const Text('2'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '3';
+                      });
+                    },
+                    child: Text('3'),
                   ),
-                  onPressed: () {
-                    _controller.text += '3';
-                  },
-                  child: const Text('3'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '-';
+                      });
+                    },
+                    child: Text('-'),
                   ),
-                  onPressed: () {
-                    _controller.text += '-';
-                  },
-                  child: const Text('-'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '0';
+                      });
+                    },
+                    child: Text('0'),
                   ),
-                  onPressed: () {
-                    _controller.text += '0';
-                  },
-                  child: const Text('0'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '.';
+                      });
+                    },
+                    child: Text('.'),
                   ),
-                  onPressed: () {
-                    _controller.text += '.';
-                  },
-                  child: const Text('.'),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '=';
+                        _result = _calculate(_expression);
+                      });
+                    },
+                    child: Text('='),
                   ),
-                  onPressed: () {
-                    _controller.text += '=';
-                  },
-                  child: const Text('='),
-                ),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    side: const BorderSide(color: Colors.grey),
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression = '';
+                        _result = '';
+                      });
+                    },
+                    child: Text('C'),
                   ),
-                  onPressed: () {
-                    _controller.text += '+';
-                  },
-                  child: const Text('+'),
-                ),
-              ],
+                  OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _expression += '+';
+                      });
+                    },
+                    child: Text('+'),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
       ),
     );
   }
-}
-class ThemeProvider with ChangeNotifier {
-  bool _isDarkMode = false;
-  bool get isDarkMode => _isDarkMode;
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
-    notifyListeners();
+
+  String _calculate(String expression) {
+    try {
+      return expression.replaceAll('=', '').toString();
+    } catch (e) {
+      return 'Error';
+    }
   }
 }
