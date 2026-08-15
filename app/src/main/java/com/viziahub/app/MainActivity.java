1b6b6a339f0abe0f19eea346e35155c72b8d7056
tarun225601-star <tarun225601@gmail.com>
2026-08-15 14:09:27 +0530

    Update MainActivity.java

---
+++ app/src/main/java/com/viziahub/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.viziahub.app;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
